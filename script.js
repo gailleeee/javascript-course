cf748fb3c5aac0fa349cfa722fc7ec5815084a33
gailleeee <radamar@students.national-u.edu.ph>
2025-08-22 14:46:26 +0800

    feat: add exercises inside starter

--- script.js
+++ script.js
@@ -1,2 +1,92 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+let js = "amazing";
+console.log(40 +8 + 23 - 10);
+
+console.log("=== VARIABLES ===");
+
+//let firstName = "Arden";
+//console.log(firstName);
+
+//let age = 20;
+//age = 21;
+//console.log(age);
+
+const birthYear = 2005;
+console.log(birthYear);
+const PI = 3.1415;
+console.log(PI);
+
+const country = "Portugal";
+const language = "Portuguese";
+const population = 10;
+
+//let age = 25;
+//age = 26;
+
+let myCurrentJob = "Video editor";
+
+console.log("=== DATA TYPES ===");
+
+// Integer
+let age = 23;
+console.log(age);
+console.log(typeof age);
+
+// String
+//et firstName = "Arden";
+//console.log(firstName);
+//console.log(typeof firstName);
+
+//Boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+//Undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+//Start as a number
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//String
+dynamicVariable = "Now i'm a string";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//Boolean
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+// Exercise 1
+let myFirstName = "Arden";
+console.log(myFirstName, typeof myFirstName);
+
+let myAge = 21;
+console.log(myAge, typeof myAge);
+
+let myNumber = 18;
+console.log(myNumber, typeof myNumber);
+
+let likeJavaScript = true;
+console.log(likeJavaScript, typeof likeJavaScript);
+
+//Exercise 2
+let myScore = 200;
+console.log(myScore);
+myScore = 150;
+console.log(myScore);
+
+const points ="200";
+console.log(points);
+
+//Exercise 3
+
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
+
+
